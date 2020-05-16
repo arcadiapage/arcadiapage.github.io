3f33f319ac71be713f0425e5b87a6aae2c1d82c9
arcadiapage <cherrelledherring@gmail.com>
2020-05-16 09:20:51 -0400

    Initial commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20200515214955";
+var CACHE_NAME = "saikiransripada-20200515225522";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202005152149",
+        "/css/style.css?202005152255",
         "/"
       ]);
     })
